41e45b7259320e6b648673af34d70769fbf6a0cc
AniketShukla14 <aniket14042001@gmail.com>
2022-07-12 15:09:38 +0530

    login and logout Backend for Government website created

--- register/dummy/urls.py
+++ register/dummy/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from xml.etree.ElementInclude import include
+from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.home,name="home")
+    path('',views.home,name="home"),
+    
 ]
